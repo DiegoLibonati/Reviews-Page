2cf4c1e2f676a5f8179f6a3d3e145db5bfd10505
Die <dlibonati@leafnoise.io>
2025-10-13 23:56:14 -0300

    feat: store abstract class with sub stores

--- src/stores/reviewStore.ts
+++ src/stores/reviewStore.ts
@@ -1,64 +1,20 @@
 import { ReviewState } from "@src/entities/states";
 import { Review } from "@src/entities/app";
-import { Listener } from "@src/entities/store";
+
+import { Store } from "@src/stores/store";
 
 import reviews from "@src/constants/reviews";
 
-class ReviewStore {
-  private state: ReviewState;
-  private listeners: {
-    [K in keyof ReviewState]: Listener<ReviewState[K]>[];
-  };
-
+export class ReviewStore extends Store<ReviewState> {
   constructor(initialState: ReviewState) {
-    this.state = initialState;
-    this.listeners = {
-      reviews: [],
-      currentReview: [],
-    };
+    super(initialState);
   }
 
-  getState(): ReviewState {
-    return this.state;
-  }
-
-  get<K extends keyof ReviewState>(key: K): ReviewState[K] {
-    return this.state[key];
-  }
-
-  setState(newState: Partial<ReviewState>): void {
-    const prevState = this.state;
-    this.state = { ...this.state, ...newState };
-
-    (Object.keys(newState) as (keyof ReviewState)[]).forEach(
-      <K extends keyof ReviewState>(key: K) => {
-        const oldValue = prevState[key];
-        const newValue = this.state[key];
-        if (oldValue !== newValue) {
-          this.listeners[key].forEach((listener) => listener(newValue));
-        }
-      }
-    );
-  }
-
-  subscribe<K extends keyof ReviewState>(
-    key: K,
-    listener: Listener<ReviewState[K]>
-  ): () => void {
-    this.listeners[key].push(listener);
-
-    return () => {
-      const arr = this.listeners[key];
-      const filtered = arr.filter((l) => l !== listener);
-      (this.listeners[key] as Listener<ReviewState[K]>[]) = filtered;
-    };
-  }
-
-  setCurrentReview(review: Review): void {
+  public setCurrentReview(review: Review): void {
     this.setState({ currentReview: review });
   }
 
-  setReviews(reviews: Review[]): void {
+  public setReviews(reviews: Review[]): void {
     this.setState({ reviews });
   }
 }
