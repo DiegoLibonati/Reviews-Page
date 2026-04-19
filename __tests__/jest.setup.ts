3de46e27ef608d6b1fd7e869bea2bfd47901ab62
Die <dlibonati@leafnoise.io>
2026-04-18 23:02:54 -0300

    fix: clean jest.setup.ts

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -1,8 +1,1 @@
 import "@testing-library/jest-dom";
-
-import { mockReviews } from "@tests/__mocks__/reviews.mock";
-
-jest.mock("@/constants/reviews", () => ({
-  __esModule: true,
-  default: mockReviews,
-}));
